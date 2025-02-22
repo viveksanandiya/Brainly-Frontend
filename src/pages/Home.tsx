ec1b58b5373706a36b4afbbed177b125b62cf5b7
Vivek-Sanandiya <viveksanandiya787@gmail.com>
2025-02-22 12:03:22 +0530

    updates in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { TypeAnimation } from "react-type-animation";
 
 export function Home() {
   return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center bg-gradient-to-r from-[#9492db] to-[#7164c0] text-white">
+    <div className="h-screen w-screen flex flex-col items-center justify-center bg-gradient-to-r from-purple-200 to-purple-600 text-white">
       <h1 className="text-4xl font-bold mb-4">
       <TypeAnimation
         sequence={["Welcome to Brainly", 2000, "", 1000]}
