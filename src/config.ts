72985440be8fa0e20736e6aee9c0cc1ce2791fc9
Vivek Sanandiya <143324091+viveksanandiya@users.noreply.github.com>
2025-05-10 19:38:49 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,4 @@
-const config = {
-    BACKEND_URL : process.env.BACKEND_URL,
-    SHAREBRAIN_URL: process.env.SHAREBRAIN_URL
-};
 
-export default config;
+export const BACKEND_URL = process.env.BACKEND_URL
+export const SHAREBRAIN_URL = process.env.SHAREBRAIN_URL
+
