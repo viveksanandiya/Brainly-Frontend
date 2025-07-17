782c8979408f89fc1cb72adf72082d48720f4b1a
Vivek-Sanandiya <viveksanandiya787@gmail.com>
2025-07-17 16:49:25 +0530

    .env --changes

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,2 @@
-const config = {
-    BACKEND_URL : process.env.BACKEND_URL,
-    SHAREBRAIN_URL: process.env.SHAREBRAIN_URL
-};
-
-export default config;
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+export const SHAREBRAIN_URL = import.meta.env.VITE_SHAREBRAIN_URL;
